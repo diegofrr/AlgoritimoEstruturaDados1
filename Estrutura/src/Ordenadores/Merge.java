07174ea1b3a76a037c9140b72eacfded5dfbd546
Diêgo <Digo@DESKTOP-D40HN2E>
2020-10-29 13:07:24 -0300

    ordenadores

--- Estrutura/src/Ordenadores/Merge.java
+++ Estrutura/src/Ordenadores/Merge.java
@@ -1,4 +1,4 @@
-package Merge;
+package Ordenadores;
 
 import java.util.Scanner;
 
--- Estrutura/src/Ordenadores/Quicksort.java
+++ Estrutura/src/Ordenadores/Quicksort.java
@@ -1,4 +1,5 @@
-package Quicksort;
+package Ordenadores;
+
 import java.util.Random;
 import java.util.Scanner;
 
